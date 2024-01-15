01e03c108bda8334151bdcaa561c615cd212f2b7
Lewe Ohlsen <mail@lewe.io>
2024-01-15 23:12:12 +0100

    Display version in app

--- src/HUD.tsx
+++ src/HUD.tsx
@@ -1,12 +1,21 @@
 import React, { useContext } from 'react';
 import { HUDContext } from './HUDContext';
+import { version } from '../package.json';
 
 const HUD: React.FC = () => {
     const hudContext = useContext(HUDContext);
 
     return (
         <div style={{ position: 'absolute', top: 10, left: 10, color: 'white' }}>
-            Closest Genre: {hudContext?.closestGenre}
+            {hudContext?.closestGenre}
+            <div style={{
+                position: 'fixed',
+                right: 0,
+                bottom: 0,
+                margin: '10px'
+            }}>
+                v{version}
+            </div>
         </div>
     );
 };
