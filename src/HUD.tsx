f6037c1f6ec9359111a215db094bd30fe0f4d7a2
Lewe Ohlsen <mail@lewe.io>
2024-04-04 20:40:25 +0200

    FPS stats

--- src/HUD.tsx
+++ src/HUD.tsx
@@ -1,22 +1,26 @@
 import React, { useContext } from 'react';
 import { HUDContext } from './HUDContext';
 import { version } from '../package.json';
+import FPSStats from "react-fps-stats";
 
 const HUD: React.FC = () => {
     const hudContext = useContext(HUDContext);
 
     return (
-        <div style={{ position: 'absolute', top: 10, left: 10, color: 'white' }}>
-            {hudContext?.closestGenre}
-            <div style={{
-                position: 'fixed',
-                right: 0,
-                bottom: 0,
-                margin: '10px'
-            }}>
-                v{version}
+        <>
+            <div style={{ position: 'absolute', top: 10, left: 10, color: 'white' }}>
+                {hudContext?.closestGenre}
+                <div style={{
+                    position: 'fixed',
+                    right: 0,
+                    bottom: 0,
+                    margin: '10px'
+                }}>
+                    v{version}
+                </div>
             </div>
-        </div>
+            <FPSStats top="auto" right="auto" bottom={10} left={10}/>
+        </>
     );
 };
 
